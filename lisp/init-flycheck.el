8e86b0ace271ac06c76c2a17ef6029e867b3fe45
HamzehN <hamzehn@github.com>
2021-01-03 18:16:50 -0500

    Change to avoid doing (require 'flycheck)

--- lisp/init-flycheck.el
+++ lisp/init-flycheck.el
@@ -2,19 +2,18 @@
 ;;; Commentary:
 ;;; Code:
 
-(require 'flycheck)
-
 (when (maybe-require-package 'flycheck)
   (add-hook 'after-init-hook 'global-flycheck-mode)
   (setq flycheck-display-errors-function #'flycheck-display-error-messages-unless-error-list)
-  ;; disable jshint since we prefer eslint checking
-  ;; http://codewinds.com/blog/2015-04-02-emacs-flycheck-eslint-jsx.html
-  (setq flycheck-disabled-checkers
-        (append flycheck-disabled-checkers
-                '(javascript-jshint)))
-  ;; customize flycheck temp file prefix
-  ;; http://codewinds.com/blog/2015-04-02-emacs-flycheck-eslint-jsx.html
-  (setq-default flycheck-temp-prefix ".flycheck")
+  (with-eval-after-load 'flycheck
+    ;; disable jshint since we prefer eslint checking
+    ;; http://codewinds.com/blog/2015-04-02-emacs-flycheck-eslint-jsx.html
+    (setq flycheck-disabled-checkers
+          (append flycheck-disabled-checkers
+                  '(javascript-jshint)))
+    ;; customize flycheck temp file prefix
+    ;; http://codewinds.com/blog/2015-04-02-emacs-flycheck-eslint-jsx.html
+    (setq-default flycheck-temp-prefix ".flycheck"))
   (when (maybe-require-package 'flycheck-color-mode-line)
     (add-hook 'flycheck-mode-hook 'flycheck-color-mode-line-mode)))
 
