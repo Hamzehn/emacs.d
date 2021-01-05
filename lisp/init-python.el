023eea7a9e193462b3d33df9fc5bfda36eb9f349
HamzehN <hamzehn@github.com>
2021-01-04 19:35:42 -0500

    General cleanup to init-python

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -2,30 +2,6 @@
 ;;; Commentary:
 ;;; Code:
 
-;; Elpy for Python
-(require-package 'elpy)
-(elpy-enable)
-;; (when (require 'flycheck nil t)
-(when (maybe-require-package 'flycheck)
-  (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
-  (add-hook 'elpy-mode-hook 'flycheck-mode))
-;;(remove-hook 'elpy-modules 'elpy-module-flymake)
-
-;; Autopep8
-;; (require-package 'py-autopep8)
-;; (add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)
-
-;; ---------------------------------
-;; Emacs IPython Notebook EIN config
-;; ---------------------------------
-(require-package 'ein)
-;; use autocompletion, but don't start to autocomplete after a dot
-(setq ein:complete-on-dot -1)
-(setq ein:use-auto-complete t)
-
-;; timeout settings
-(setq ein:query-timeout 1000)
-
 (setq auto-mode-alist
       (append '(("SConstruct\\'" . python-mode)
                 ("SConscript\\'" . python-mode))
@@ -33,7 +9,27 @@
 
 (setq python-shell-interpreter "python3")
 
-(require-package 'pip-requirements)
+;; Use Elpy for Python (alternative would be anaconda-mode)
+(when (maybe-require-package 'elpy)
+  (elpy-enable)
+  (setq elpy-rpc-virtualenv-path 'default)
+  (when (maybe-require-package 'flycheck)
+    (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
+    (add-hook 'elpy-mode-hook 'flycheck-mode))
+  (setq elpy-rpc-backend "jedi"))
+
+;; Emacs IPython Notebook EIN config
+(when (maybe-require-package 'ein)
+  ;; use autocompletion, but don't start to autocomplete after a dot
+  (setq ein:complete-on-dot -1)
+  (setq ein:use-auto-complete t)
+  (setq ein:query-timeout 1000))
+
+(when (maybe-require-package 'toml-mode)
+  (add-to-list 'auto-mode-alist '("poetry\\.lock\\'" . toml-mode)))
+
+(when (maybe-require-package 'reformatter)
+  (reformatter-define black :program "black"))
 
 (provide 'init-python)
 ;;; init-python.el ends here
